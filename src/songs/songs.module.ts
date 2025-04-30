aadf70d3110c88b88ccf50b4fc654a03868e2b27
Simone Taeggi <startae14@gmail.com>
2025-05-01 00:46:21 +0200

    Value Provider mocked

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,13 +2,29 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 
+const mockSongsService = {
+  findAll() {
+    return [
+      {
+        id: 1,
+        title: 'Lasting lover',
+      },
+    ];
+  },
+};
+
 @Module({
   controllers: [SongsController],
   // providers: [SongsService],
   providers: [
+    SongsService,
+    // {
+    //   provide: SongsService,
+    //   useClass: SongsService,
+    // },
     {
       provide: SongsService,
-      useClass: SongsService,
+      useValue: mockSongsService,
     },
   ],
 })

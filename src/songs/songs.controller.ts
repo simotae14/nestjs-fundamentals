f0ec6a4150c42e888712a7797ebbdaafd793a31d
Simone Taeggi <startae14@gmail.com>
2025-04-30 20:20:37 +0200

    Add Validation with class-validator

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Delete, Get, Put, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Controller, Delete, Get, Put, Post, Body } from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
   constructor(private songsService: SongsService) {}
   @Post()
-  create() {
-    return this.songsService.create('Animals by Martin Garrix');
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.create(createSongDTO);
   }
 
   @Get()
@@ -25,7 +27,7 @@ export class SongsController {
   }
 
   @Delete(':id')
-  remove(): string {
+  delete(): string {
     return 'delete song on the based on id';
   }
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SongsService {
   // local DB
   // local array of songs
-  private readonly songs: string[] = [];
+  private readonly songs: any = [];
 
   // create a new song
-  create(song: string) {
+  create(song) {
     // Save the song in the DB
     this.songs.push(song);
     return this.songs;
